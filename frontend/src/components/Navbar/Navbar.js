95d1ce60577d8e5e9f228a46c5abecd8e9984ad8
sowmyagunturi <mythilidurga772@gmail.com>
2025-09-18 23:55:49 +0530

    Update Navbar.js

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
 
   const handleLogout = async () => {
     try {
-      await fetch("https://lead-management-bend.onrender.com/auth/logout", {
+      await fetch("https://lead-management-a.onrender.com/auth/logout", {
         method: "POST",
         credentials: "include",
       });
