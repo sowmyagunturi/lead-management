b047779fb428e5db0a21424143c0264e850961ce
sowmyagunturi <mythilidurga772@gmail.com>
2025-09-18 23:03:10 +0530

    Update Signup.js

--- frontend/src/pages/Signup/Signup.js
+++ frontend/src/pages/Signup/Signup.js
@@ -19,7 +19,7 @@ const Signup = () => {
     e.preventDefault(); // stop page reload
 
     try {
-      const res = await fetch("http://localhost:8080/auth/register", {
+      const res = await fetch("https://lead-management-bend.onrender.com/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
